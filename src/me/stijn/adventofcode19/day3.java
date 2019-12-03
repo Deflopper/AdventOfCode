5d13c8c5ad1128d47c4f95a09bd65dc8f098d900
stijn-jacobs <stijn.jacobs33@gmail.com>
2019-12-03 14:07:56 +0100

    Added day 3 part 2

--- src/me/stijn/adventofcode19/day3.java
+++ src/me/stijn/adventofcode19/day3.java
@@ -11,7 +11,42 @@ public class day3 {
 	public static void main(String[] args) throws IOException {
 		ArrayList<String> input = Utils.getInput(3),path = new ArrayList();
 		
-		HashMap<Point2D, Character> map = new HashMap(); 
+		HashMap<Point2D, GridCell> map = new HashMap(); 
+		double x,y;
+		int iteration;
+		
+		for (int i = 0; i < 2; i++) {
+			String line = input.get(i);
+			x = 0;
+			y = 0;
+			iteration = 1;
+			for (String s : line.split(",")) {
+				char dir = s.charAt(0);
+				int amount = Integer.valueOf(s.substring(1));
+				Point2D p = addToPath(map, new Point2D.Double(x,y), amount, dir, (char)(i + '0'), iteration);
+				x = p.getX();
+				y = p.getY();
+				iteration+=amount;
+			}
+		}
+		
+		//calculate closest intersection
+		int smallest = Integer.MAX_VALUE;
+		for (Point2D p : map.keySet()) {
+			if (map.get(p).c == 'X') {
+				if (map.get(p).steps < smallest) {
+					smallest = map.get(p).steps;
+				}
+			}
+		}
+
+		System.out.println("Smallest intersection after: " + smallest + " steps");
+	}
+
+	public static void part1(String[] args) throws IOException {
+		ArrayList<String> input = Utils.getInput(3),path = new ArrayList();
+		
+		HashMap<Point2D, GridCell> map = new HashMap(); 
 		double x,y;
 		
 		for (int i = 0; i < 2; i++) {
@@ -21,7 +56,7 @@ public class day3 {
 			for (String s : line.split(",")) {
 				char dir = s.charAt(0);
 				int amount = Integer.valueOf(s.substring(1));
-				Point2D p = addToPath(map, new Point2D.Double(x,y), amount, dir, (char)(i + '0'));
+				Point2D p = addToPath(map, new Point2D.Double(x,y), amount, dir, (char)(i + '0'),0);
 				x = p.getX();
 				y = p.getY();
 			}
@@ -30,7 +65,7 @@ public class day3 {
 		//calculate closest intersection
 		int smallest = Integer.MAX_VALUE;
 		for (Point2D p : map.keySet()) {
-			if (map.get(p) == 'X') {
+			if (map.get(p).c == 'X') {
 				int distance = (int) (Math.abs(p.getX()) + Math.abs(p.getY()));
 				if (distance < smallest)
 					smallest = distance;
@@ -40,7 +75,27 @@ public class day3 {
 		System.out.println("Smallest distance to a crossing point: " + smallest);
 	}
 	
-	public static Point2D addToPath(HashMap<Point2D, Character> map, Point2D.Double p1, int amount, char dir, char c) {
+	public static class GridCell{
+		char c;
+		int steps;
+		
+		public GridCell(int steps, char c) {
+			this.c = c;
+			this.steps = steps;
+		}
+	}
+	
+	/**
+	 * Add the gridcells to the given map
+	 * @param map to add the gridcells to
+	 * @param p1 starting point
+	 * @param amount amount of cells to add
+	 * @param dir direction in which to add the cells
+	 * @param c character to add into the cells
+	 * @param globalItteration number of global iterations before this operation
+	 * @return last point added to the map
+	 */
+	public static Point2D addToPath(HashMap<Point2D, GridCell> map, Point2D.Double p1, int amount, char dir, char c, int globalItteration) {
 		int x = (int) p1.x, y = (int) p1.y, iteration = 0;
 		while (iteration != amount) {
 			switch (dir) {
@@ -54,10 +109,10 @@ public class day3 {
 				break;
 			}
 			Point2D p = new Point2D.Double(x, y);
-			if (map.containsKey(p) && map.get(p) != c) 
-				map.put(p, 'X');
-			else 
-				map.put(p, c);
+			if (map.containsKey(p) && map.get(p).c != c) 
+				map.put(p, new GridCell(globalItteration + iteration + map.get(p).steps, 'X'));
+			else if (!map.containsKey(p))
+				map.put(p, new GridCell(globalItteration + iteration, c));
 			iteration++;
 		}
 		return new Point2D.Double(x,y);
